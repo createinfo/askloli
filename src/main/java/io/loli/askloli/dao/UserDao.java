a226df4d5f75087ebfe63b67ec7de28dd496e387
choco <uzumakitenye@gmail.com>
2013-08-14 23:13:35 +0800

    Maven dependency done. fixed #1 Add tomcat7 dependency Add user entity and dao/test

---
+++ src/main/java/io/loli/askloli/dao/UserDao.java
@@ -0,0 +1,9 @@
+package io.loli.askloli.dao;
+
+import io.loli.askloli.entity.User;
+
+public interface UserDao {
+
+    void save(User user);
+
+}
---
+++ src/main/java/io/loli/askloli/entity/User.java
@@ -0,0 +1,58 @@
+package io.loli.askloli.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ * 用户信息的实体
+ * 
+ * @author choco
+ * 
+ */
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private Integer id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(length = 32, nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
