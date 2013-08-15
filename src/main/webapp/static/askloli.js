10504e1e8de9290d46f15e5c0f2aeed43b0cd4e2
orangel-yc <or.ange.l.evol@gmail.com>
2013-08-15 17:52:21 +0800

    update login

--- src/main/webapp/static/askloli.js
+++ src/main/webapp/static/askloli.js
@@ -1,9 +1,19 @@
 //global
 
 function getcoockie(){
-  return false;
+  if (document.cookie.length>0){
+    u_start=document.cookie.indexOf("uname=");
+    p_start=document.cookie.indexOf("pw=");
+	if (u_start != -1 && p_start != -1){
+	  return {"username":document.cookie.substring(u_start + 6,p_start), "pw":document.cookie.substring(p_start + 3)}
+	}
+	else{return false;};
+  };
 };
 
-function setcoockie(){
-  
+function setcoockie(uname, md5){
+  document.cookie = "uname=" + uname + "pw=" + md5;
+};
+
+function autologin(){
 };
