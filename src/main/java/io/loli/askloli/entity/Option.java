9c5108f6470771075e8c83fd6f35e5b0f2b92c4f
chocotan <uzumakitenye@gmail.com>
2013-08-20 17:37:17 +0800

    Add survey entities

---
+++ src/main/java/io/loli/askloli/entity/Option.java
@@ -0,0 +1,53 @@
+package io.loli.askloli.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Option {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String key;
+    private String value;
+
+    @ManyToOne
+    @JoinColumn(name="question_id")
+    private Question question;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+}
---
+++ src/main/java/io/loli/askloli/entity/Survey.java
@@ -0,0 +1,59 @@
+package io.loli.askloli.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Survey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String title;
+
+    @ManyToOne(cascade = CascadeType.REFRESH)
+    @JoinColumn(name = "creator_id", nullable = false)
+    private User creator;
+    
+    @OneToMany(cascade = CascadeType.ALL)
+    private List<Question> questions;
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getCreator() {
+        return creator;
+    }
+
+    public void setCreator(User creator) {
+        this.creator = creator;
+    }
+
+    public Question getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Question questions) {
+        this.questions = questions;
+    }
+
+}
